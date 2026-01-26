4a228cf28f6c1c94d9eaaf143aa0c14d12419f0a
McCarryster <michinemi964@gmail.com>
2026-01-26 23:54:30 +0400

    kubernetes setup work

--- vector_db_service/app/config.py
+++ vector_db_service/app/config.py
@@ -9,7 +9,8 @@ COLLECTION_NAME: str = os.getenv("COLLECTION_NAME", "arxiv_papers_docs")
 INDEX_NAME: str = os.getenv("INDEX_NAME", "arxiv_papers_index")
 
 
-WEAVIATE_HTTP_HOST: str = os.getenv("WEAVIATE_HOST", "weaviate")
-WEAVIATE_HTTP_PORT: int = int(os.getenv("WEAVIATE_PORT", "8080"))
-WEAVIATE_GRPC_HOST: str = os.getenv("WEAVIATE_HOST", "weaviate")
+WEAVIATE_HTTP_HOST: str = os.getenv("WEAVIATE_HTTP_HOST", "weaviate")
+WEAVIATE_HTTP_PORT: int = int(os.getenv("WEAVIATE_HTTP_PORT", "8080"))
+
+WEAVIATE_GRPC_HOST: str = os.getenv("WEAVIATE_GRPC_HOST", "weaviate")
 WEAVIATE_GRPC_PORT: int = int(os.getenv("WEAVIATE_GRPC_PORT", "50051"))
